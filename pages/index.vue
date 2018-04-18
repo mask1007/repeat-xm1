8c30ce379fd80cd437fa9946785eaa4dd65432f1
mask1007 <1905180201@qq.com>
2018-04-18 22:55:22 +0800

    项目一新建登录注册页面，登录样式写了80%

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>hello</p>
+    </div>
+</template>
+
+<script>
+    export default {
+      data(){
+          return{
+              name:'首页'
+          }
+
+      }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
